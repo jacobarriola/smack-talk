f1555f6000cb7f40d96b14fde6d49b16e5071c5f
Jacob Arriola <jacob.arriola@gmail.com>
2014-10-07 14:07:29 -0700

    more stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,27 @@
-var gulp = require('gulp');
+gulpvar gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+var sass        = require('gulp-sass');
 
-gulp.task('default', function() {
-  // place code for your default task here
+// browser-sync task for starting the server.
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+// Sass task, will run when any SCSS files change & BrowserSync
+// will auto-update browsers
+gulp.task('sass', function () {
+    return gulp.src('assets/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('.'))
+        .pipe(reload({stream:true}));
+});
+
+// Default task to be run with `gulp`
+gulp.task('default', ['sass', 'browser-sync'], function () {
+    gulp.watch("assets/sass/*.scss", ['sass']);
 });
